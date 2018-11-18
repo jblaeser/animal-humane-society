38303b698f71ae52948c1a449959d1c4f747e96b
Jill Blaeser <jblaeser01@gmail.com>
2018-11-18 15:51:14 -0600

    Location On

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var locationLink = document.querySelector('.locationlink');
+locationLink.addEventListener('click', function() {console.log('Open Sesame');
+document.querySelector('body')
+    .classList
+    .toggle('location-on')
+});
+
