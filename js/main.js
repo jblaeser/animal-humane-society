04a341712da31b22dba3a70d8f32d9b262ecfa76
Jill Blaeser <jblaeser01@gmail.com>
2018-11-18 16:45:46 -0600

    Adoption Form

--- js/main.js
+++ js/main.js
@@ -5,3 +5,31 @@ document.querySelector('body')
     .toggle('location-on')
 });
 
+
+var adoptButton1 = document.querySelector('.adoptbutton1');
+adoptButton1.addEventListener('click', function() {console.log('Open Sesame');
+document.querySelector('body')
+    .classList
+    .toggle('adoption-open')
+});
+
+var adoptButton2 = document.querySelector('.adoptbutton2');
+adoptButton2.addEventListener('click', function() {console.log('Open Sesame');
+document.querySelector('body')
+    .classList
+    .toggle('adoption-open')
+});
+
+var adoptButton3 = document.querySelector('.adoptbutton3');
+adoptButton3.addEventListener('click', function() {console.log('Open Sesame');
+document.querySelector('body')
+    .classList
+    .toggle('adoption-open')
+});
+
+var closeButton = document.querySelector('.close-button');
+closeButton.addEventListener('click', function() {console.log('Close Sesame');
+document.querySelector('body')
+    .classList
+    .toggle('adoption-open')
+});
